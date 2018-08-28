18256a2f6cfaec016322b7dc4cc6986c6ac0384a
MikeBLambert <mlamb128@gmail.com>
2018-08-28 16:42:33 -0700

    Import Api to Countries and pass data to Country

--- app/src/components/countries/Countries.vue
+++ app/src/components/countries/Countries.vue
@@ -2,19 +2,35 @@
     <section>
         <h2>Countries Visited</h2>
         <ul>
-            <Country/>
+            <Country
+                v-for="(country, index) in countries"
+                :key="index"
+                :country="country"
+            />
         </ul>
         <AddCountry/>
     </section>
 </template>
 
 <script>
-import Country from './Country';
-import AddCountry from './AddCountry';
+import Country from './Country.vue';
+import AddCountry from './AddCountry.vue';
+import api from '../../services/api';
 
 
 export default {
-    compoonents: {
+    data() {
+        return {
+            countries: null
+        };
+    },
+    created() {
+        api.getCountries()
+            .then(countries => { 
+                this.countries = countries;
+            });
+    },
+    components: {
         Country,
         AddCountry
     },

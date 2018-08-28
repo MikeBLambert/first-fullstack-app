26e760f6ed9c0387c9c1310904a4cffbd8a0f33a
MikeBLambert <mlamb128@gmail.com>
2018-08-28 14:47:30 -0700

    Fix issue with error, add component dependencies in app, and create data json of countries

---
+++ app/src/components/countries/Countries.vue
@@ -0,0 +1,26 @@
+<template>
+    <section>
+        <h2>Countries Visited</h2>
+        <ul>
+            <Neighborhood/>
+        </ul>
+        <AddCountry/>
+    </section>
+</template>
+
+<script>
+import Country from './Country';
+import AddCountry from './AddCountry';
+
+
+export default {
+compoonents: {
+    Country,
+    AddCountry
+}
+}
+</script>
+
+<style>
+
+</style>
--- app/src/router.js
+++ app/src/router.js
@@ -1,10 +1,10 @@
 import VueRouter from 'vue-router';
-import Home from '.components/home/Home.vue'
-import Countries from '.components/countries/Countries.vue'
+import Home from './components/home/Home.vue';
+import Countries from './components/countries/Countries.vue';
 
 export default new VueRouter({
     routes:[
-        { path: '/', component Home },
+        { path: '/', component: Home },
         { path: '/countries', component: Countries },
         { path: '*', redirect: '/' }
     ]

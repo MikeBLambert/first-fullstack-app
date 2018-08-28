4df5e794dc91ee2b5044681e90f3661b2f84cf54
MikeBLambert <mlamb128@gmail.com>
2018-08-28 16:41:59 -0700

    Add html form and radio buttons and css styling to template

--- app/src/components/countries/AddCountry.vue
+++ app/src/components/countries/AddCountry.vue
@@ -1,5 +1,20 @@
 <template>
-    <section></section>
+    <section class="add-country">
+        <h1>Add New Country</h1>
+        <form>
+            <label>
+                Country Name: <input type="text" name="name" placeholder="name">
+            </label>
+            <label>
+                Population: <input type="number" name="population" placeholder="population">
+            </label>
+            <label>
+                Visited?: 
+                Yes <input type="radio" name="visited" value="yes">
+                No <input type="radio" name="visited" value="no">
+            </label>
+        </form>
+    </section>
 </template>
 
 <script>
@@ -10,4 +25,15 @@ export default {
 
 <style>
 
+.add-country {
+  width: 300px;
+  text-align: left;
+  margin: auto;
+}
+
+label {
+  display: block;
+  margin: 25px;
+  font-size: 25px;
+}
 </style>

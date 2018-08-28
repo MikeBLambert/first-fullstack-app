a0bf06b21ee4b7930ea5d8669fd69adf7d3daa05
MikeBLambert <mlamb128@gmail.com>
2018-08-28 16:43:18 -0700

    Add scaffold to Country.vue with country property passed to component from Countries

---
+++ app/src/components/countries/Country.vue
@@ -0,0 +1,15 @@
+<template>
+    <article>
+        <h3>{{ country.name }}</h3>
+    </article>
+</template>
+
+<script>
+export default {
+    props: ['country']
+}
+</script>
+
+<style>
+
+</style>
--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,8 +1,9 @@
-// export default {
-//     getCountries() {
-//         return fetch('http://localhost:3000/api/countries', {
-//             headers: { 'Content-Type' : 'application/json' }
-//         })
-//             .then(response =>)
-//     }
-// }
+export default {
+    getCountries() {
+        return fetch('http://localhost:3000/api/countries', {
+            headers: { 'Content-Type' : 'application/json' }
+        })
+            .then(response => response.json())
+            .then(console.log("yo"));
+    }
+}

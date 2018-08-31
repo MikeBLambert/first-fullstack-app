4e86642b93bcaa887ae84dd4e97623e6301d447a
MikeBLambert <mlamb128@gmail.com>
2018-08-31 16:11:13 -0700

    Populate scaffold of Country form. Still working on handleSubmit method

---
+++ app/src/components/countries/CountryForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <form @submit.prevent="handleSubmit">
+            <h2>{{ country.name }}</h2>
+            <label>
+                No. of Visits: <input type="number" name="visits">
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        country: Object
+    },
+    methods: {
+        handleSubmit() {
+            // not right.
+            this.onEdit(XXXX)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

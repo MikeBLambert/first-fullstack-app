ccbe4743f8f4b4d6ac13898cb79348909d405c3e
MikeBLambert <mlamb128@gmail.com>
2018-08-30 18:25:37 -0700

    Refactor api fetch URLs and add fetch function for languages called getLanguages

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,21 +1,31 @@
+const URL = 'http://localhost:3000/api';
+const COUNTRY_VISIT_URL =  `${URL}/country-visit-info`
+const LANGUAGES_URL = `${URL}/languages` 
+
 export default {
   getCountries() {
-    return fetch('http://localhost:3000/api/country-visit-info', {
+    return fetch(COUNTRY_VISIT_URL, {
       headers: { 'Content-Type' : 'application/json' }
     })
       .then(response => response.json());
   },
   getCountry(id) {
-    return fetch(`http://localhost:3000/api/country-visit-info/${id}`, {
+    return fetch(`${COUNTRY_VISIT_URL}/${id}`, {
       headers: { 'Content-Type' : 'application/json' }
     })
       .then(response => response.json());
   },
   addCountry(country) {
-    return fetch('http://localhost:3000/api/country-visit-info', {
+    return fetch(COUNTRY_VISIT_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(country)
+    })
+      .then(response => response.json());
+  },
+  getLanguages() {
+    return fetch(LANGUAGES_URL, {
+      headers: { 'Content-type' : 'application/json' }
     })
       .then(response => response.json());
   }

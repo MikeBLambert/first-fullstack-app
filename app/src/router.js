76fdcd037bfe80ea7b88f9bf9b9f881bd402970b
MikeBLambert <mlamb128@gmail.com>
2018-08-30 18:23:42 -0700

    Connect AddCountry to router

--- app/src/router.js
+++ app/src/router.js
@@ -2,11 +2,13 @@ import VueRouter from 'vue-router';
 import Home from './components/home/Home.vue';
 import Countries from './components/countries/Countries.vue';
 import CountryDetail from './components/countries/CountryDetail.vue';
+import AddCountry from './components/countries/AddCountry.vue';
 
 export default new VueRouter({
   routes:[
     { path: '/', component: Home },
     { path: '/countries', component: Countries },
+    { path: '/countries/add', component: AddCountry },
     { path: '/countries/:id', component: CountryDetail },
     { path: '*', redirect: '/' },
   ]

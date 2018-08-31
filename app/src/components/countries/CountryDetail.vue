61f2914910a1f44003dfae7cba73322361cb2ec8
MikeBLambert <mlamb128@gmail.com>
2018-08-31 16:10:28 -0700

    Add CountryForm component, revise how country details show in GUI, and add edit and delete buttons

--- app/src/components/countries/CountryDetail.vue
+++ app/src/components/countries/CountryDetail.vue
@@ -1,25 +1,48 @@
 <template>
-    <main v-if="country">
-        <div></div>
-        <h2>You have been to {{country.name}} {{country.times}} times.</h2>
-    </main>
+    <article v-if="!editing">
+        <main v-if="country">
+            <h2>{{country.name}}</h2>
+            <h2>Number of Visits: {{country.times}}</h2>
+            <h2>Primary Country Language: {{country.language}}</h2>
+            <button @click="editing = !editing">{{ editing ? 'Cancel' : 'Edit'}}</button>
+            <button>Delete</button>
+        </main>
+    </article>
+    <CountryForm v-else
+        :country="country"
+        :onEdit="handleUpdate"/>
 </template>
 
 <script>
 
 import api from '../../services/api';
+import CountryForm from './CountryForm';
 
 export default {
     data() {
         return {
             country: null,
+            editing: false
         }
     },
     created() {
         api.getCountry(this.$route.params.id) 
             .then(country => { 
-                this.country = country        
+                this.country = country   
             })
+    },
+    components: {
+        CountryForm,
+    },
+    methods: {
+        handleUpdate(toUpdate) {
+            return api.updateCountry(toUpdate)
+                .then(update => {
+                    this.country = update;
+                    this.edit = false;
+                })
+        }
+
     }
 }
 
